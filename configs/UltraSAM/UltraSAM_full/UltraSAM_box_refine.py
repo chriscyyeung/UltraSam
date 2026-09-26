10a533da690b6ec0686854b394f12018efbec7bd
Chris Yeung <chriscyyeung@gmail.com>
2026-09-26 10:07:41 -0400

    fix data paths

--- configs/UltraSAM/UltraSAM_full/UltraSAM_box_refine.py
+++ configs/UltraSAM/UltraSAM_full/UltraSAM_box_refine.py
@@ -1,15 +1,15 @@
 _base_ = ['../../_base_/datasets/sam_dataset_bbox_prompt.py', '../../_base_/models/sam_mask_refinement.py']
 
-data_root = '/home/cyeung/projects/aip-medilab/cyeung/LumpNavPNGResMatched'
+data_root = '/home/cyeung/projects/aip-medilab/cyeung/LumpNavPNGResMatchedCV/fold_4'
 
 metainfo = dict(classes=('tumor',))
 
 train_dataloader = dict(
-    batch_size=8,
+    batch_size=1,
     dataset=dict(
         data_root=data_root,
-        data_prefix=dict(img=''),
-        ann_file='train.agnostic.noSmall.coco.json',
+        data_prefix=dict(img='train/images'),
+        ann_file='train/coco_annotations.json',
         metainfo=metainfo,
     ),
 )
@@ -17,8 +17,8 @@ train_dataloader = dict(
 val_dataloader = dict(
     dataset=dict(
         data_root=data_root,
-        data_prefix=dict(img='images'),
-        ann_file='coco_annotations.json',
+        data_prefix=dict(img='val/images'),
+        ann_file='val/coco_annotations.json',
         metainfo=metainfo,
     ),
 )
@@ -26,17 +26,18 @@ val_dataloader = dict(
 test_dataloader = dict(
     dataset=dict(
         data_root=data_root,
-        data_prefix=dict(img='images'),
-        ann_file='coco_annotations_nonempty.json',
+        data_prefix=dict(img='val/images'),
+        ann_file='val/coco_annotations.json',
         metainfo=metainfo,
     ),
 )
 
 orig_val_evaluator = _base_.val_evaluator
-orig_val_evaluator['ann_file'] = '{}/coco_annotations.json'.format(data_root)
+orig_val_evaluator['ann_file'] = '{}/val/coco_annotations.json'.format(data_root)
 val_evaluator = orig_val_evaluator
 
 orig_test_evaluator = _base_.test_evaluator
-orig_test_evaluator['ann_file'] = '{}/coco_annotations_nonempty.json'.format(data_root)
+orig_test_evaluator['ann_file'] = '{}/val/coco_annotations.json'.format(data_root)
 orig_test_evaluator['metric'] = ['segm']
+orig_test_evaluator['outfile_prefix'] = './results/fold4_preds'
 test_evaluator = orig_test_evaluator
